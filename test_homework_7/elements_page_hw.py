c6076057517acde66bd61fcfedf23b735eecfbef
Vladislav555555 <hearthstoneganster@gmail.com>
2025-09-17 21:44:32 +0300

    Delete test_homework_7/elements_page_hw.py

--- test_homework_7/elements_page_hw.py
+++
@@ -1,9 +0,0 @@
-'''from test_homework_7.base_page_hw import BasePage
-from test_homework_7.components_hw import WebElement
-
-class ElementsPage(BasePage):
-    def __init__(self, driver):
-        self.base_url = 'https://demoqa.com/elements'
-        super().__init__(driver,self.base_url)
-        self.central_text = WebElement(driver, 'div.col-12.mt-4.col-md-6')'''
-
