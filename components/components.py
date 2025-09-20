9369130d1766da457bdb6a93a9a7d9438de88cf3
Vladislav <Hearthstoneganster@gmail.com>
2025-09-20 11:33:25 +0300

    1

--- components/components.py
+++ components/components.py
@@ -12,6 +12,9 @@ class WebElement:
     def find_element(self):
         return self.driver.find_element(By.CSS_SELECTOR, self.locator)
 
+    def find_elements(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, self.locator)
+
     def get_text(self):
         return str(self.find_element().text)
 
@@ -30,3 +33,13 @@ class WebElement:
 
     def is_displayed(self):
         return self.find_element().is_displayed()
+
+    def check_count_elements(self, count: int)-> bool:
+        if len(self.find_elements()) == count:
+            return True
+        return False
+
+    def send_keys(self, text: str):
+        self.find_element().send_keys(text)
+
+
